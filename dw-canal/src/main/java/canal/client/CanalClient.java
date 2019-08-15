b3edd4d8437febdb3fdfad1e92f6ef25274d3263
Dangh <54danghao@gmail.com>
2019-08-15 14:12:15 +0800

    canal模块

---
+++ dw-canal/src/main/java/canal/client/CanalClient.java
@@ -0,0 +1,25 @@
+package canal.client;
+
+public class CanalClient {
+    public static void main(String[] args) {
+
+        //建立连接器
+
+        while (true){
+            //连接
+            //订阅
+            //抓取100条sql，抓取100份数据
+
+            //如果没有抓取到数据，冷静5秒
+
+            //有数据，单独处理
+
+                //只有行变化才处理
+
+                //提取entry数据
+                //entry数据反序列化，
+                //得到rowDataList
+                //得到表结构
+        }
+    }
+}
---
+++ dw-canal/src/main/java/canal/client/utils/KafkaSender.java
@@ -0,0 +1,6 @@
+package canal.client.utils;
+
+public class KafkaSender {
+
+    //kafka配置单独写配置文件读入
+}
